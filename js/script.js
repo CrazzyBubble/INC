b8059ce28c7e54cbc80b080600847c826f478a1b
Gmart0 <novayazemlya2015@gmail.com>
2020-06-13 22:59:13 +0300

    login/register_creation+view_likes/rates

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var button_menu_open = document.querySelector(".top__menu");
-var left_menu = document.querySelector(".menu")
+var left_menu = document.querySelector(".menu");
 
 button_menu_open.addEventListener("click", () => {
     if (left_menu.classList.contains('hidden')) {
@@ -8,3 +8,24 @@ button_menu_open.addEventListener("click", () => {
         left_menu.classList.add('hidden');
     }
 });
+
+var x = document.querySelector("select");
+var i;
+for (i = 0; i < 10; i++) {
+    var option = document.createElement("option");
+    option.text = i + 1;
+    option.classList.add("selector_style");
+    x.add(option);
+}
+
+var liked = document.querySelector(".image_like");
+liked.addEventListener("click", () => {
+    if (liked.classList.contains('on')) {
+        liked.classList.remove('on');
+    } else {
+        liked.classList.add('on');
+        liked.classList.remove("hover");
+        liked.removeAttribute("hover");
+        liked.removeAttribute
+    }
+})
